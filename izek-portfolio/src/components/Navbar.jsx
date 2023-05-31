845da98d2a5429a8498205050ad5a041c93c89d2
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-05-31 16:29:23 -0700

    added navbar component

---
+++ izek-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300">
+      <div>
+        {/* <img src={Logo} alt="logo image" style={{ width: "50px" }} /> */}
+      </div>
+
+      {/* Menu */}
+      <div className="hidden md:flex">
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="flex fixed flex-col top-[35%] left-0"></div>
+    </div>
+  );
+};
+
+export default Navbar;
