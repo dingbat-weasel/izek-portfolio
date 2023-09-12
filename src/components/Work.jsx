a66819f1cabcae60a2253adc6c43a4a23d34203c
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-09-11 22:58:49 -0700

    deploy ready, icon added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ function Work() {
   const projects = [
     {
       title: "Fast React Pizza Co.",
-      img: "src/assets/project_images/fast_react_pizza_co.JPG",
+      img: "./assets/project_images/fast_react_pizza_co.JPG",
       p1: "Online ordering platform for small pizza shop. Features dynamic menu fetched from an API, stateful shopping cart, and ordering form complete with geolocation.",
       p2: "Utilizes more advanced React techniques including data fetching with React Router, Redux Toolkit, and Thunks.",
       liveLink: "https://fast-react-pizza-izek.vercel.app/",
@@ -14,7 +14,7 @@ function Work() {
     },
     {
       title: "Monostich",
-      img: "src/assets/project_images/monostich.JPG",
+      img: "./assets/project_images/monostich.JPG",
       p1: "Full stack social media site. Users create poems using a feature that emulates fridge magnet poetry. Can share  to their page and view pages of other users.",
       p2: "Utilizes the React and MUI component library on the frontend, GraphQL, Express, and MongoDB on the backend.",
       liveLink: "https://monostich-132ed184814a.herokuapp.com/",
