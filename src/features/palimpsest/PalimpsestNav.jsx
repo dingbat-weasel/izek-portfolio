8495255291855fd245d8a85c176f9dd8fe8f6a51
dingbat-weasel <isaacrperk@gmail.com>
2023-12-03 14:12:54 -0800

    nav bar functioning

--- src/features/palimpsest/PalimpsestNav.jsx
+++ src/features/palimpsest/PalimpsestNav.jsx
@@ -1,32 +1,26 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { postData } from "./data/posts";
 
 function PalimpsestNav() {
-  let { postId: currentPostId } = useParams();
-  const { title: currentTitle, createdAt: currentCreatedAt } =
-    postData[currentPostId];
-  const { title: previousTitle, createdAt: previousCreatedAt } =
-    postData[currentPostId - 1] || {};
-  const { title: nextTitle, createdAt: nextCreatedAt } =
-    postData[currentPostId + 1] || {};
+  let { postId: currentParamId } = useParams();
 
-  const isPreviousPostAvailable = function () {
-    if (postData[currentPostId] - 1) return true;
-    return false;
-  };
+  const { id: currentId, title: currentTitle } = postData[currentParamId];
 
-  const isNextPostAvailable = function () {
-    if (postData[currentPostId] + 1) return true;
-    return false;
-  };
+  const { id: previousPostId, title: previousTitle } =
+    postData[currentId - 1] || {};
+  const { id: nextPostId, title: nextTitle } = postData[currentId + 1] || {};
 
   return (
     <div className="flex items-center justify-center gap-10 border-b-zinc-100 bg-zinc-300 px-20 py-5 ">
-      {isPreviousPostAvailable && <div>&lt;- {previousTitle}</div>}
-      <div>
-        {currentTitle}, {currentCreatedAt}
-      </div>
-      {isNextPostAvailable && <div>{nextTitle}-&gt;</div>}
+      {previousTitle && (
+        <Link to={`/palimpsest/post/${previousPostId}`}>
+          &lt;- {previousTitle}
+        </Link>
+      )}
+      <div>{currentTitle}</div>
+      {nextTitle && (
+        <Link to={`/palimpsest/post/${nextPostId}`}>{nextTitle}-&gt;</Link>
+      )}
     </div>
   );
 }
